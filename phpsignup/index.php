f23f53c154fdbbdd8a89b160472ab5fb5597390b
danieleyny <danieleyny@gmail.com>
2019-11-22 18:18:55 -0500

    basic css and html update

--- phpsignup/index.php
+++ phpsignup/index.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+    require "header.php"
+?>
+    <main class="logsituation">
+        <p>You are logged out</p>
+        <p>You are logged in</p>
+    </main>
+
+<?php
+    require "footer.php";
+?>  
