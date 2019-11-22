0fd1af87f5bb2c72b4ce0f5a7531e09329ff4436
Daniel Eyny <48768378+danieleyny@users.noreply.github.com>
2019-11-21 22:37:18 -0500

    Add files via upload

---
+++ phpsignup/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <header>
+        <nav>
+            <a href="#">
+                <img src="image/danieleynylogo1.png" alt="Daniel Eynys Logo">
+            </a>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">About Me</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <div> 
+                <form action="includes/login.inc.php" method="post">
+            </div>
+        </nav>
+    </header> 
+
+
+
+
+
+
+
+</body>
+</html>
